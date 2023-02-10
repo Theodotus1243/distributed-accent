4e3323a597a666c2516aced62a3f1bf8574e1769
Богдан Михайленко <mihaylenko.bogdan12@gmail.com>
2023-02-10 14:13:37 +0200

    Added check integrity

--- distributed_accent.py
+++ distributed_accent.py
@@ -2,6 +2,19 @@ from typing import List
 from ukrainian_word_stress import Stressifier, StressSymbol, OnAmbiguity
 import numpy as np
 
+import hashlib
+
+
+
+def hash_file(filename: str, block_size: int=2**20):
+    md5 = hashlib.md5()
+    with open(filename, "rb") as f:
+        while True:
+            data = f.read(block_size)
+            if not data:
+                break
+            md5.update(data)
+    return md5.digest()
 
 
 def stress(texts: List[str]):
@@ -14,3 +27,16 @@ def stress(texts: List[str]):
         ambiguity = (text_skip!=text_all)
         ambiguous_texts.append((text_all, ambiguity))
     return ambiguous_texts
+
+
+if (__name__ == "__main__"):
+    filename = "full.txt"
+
+
+    # check integrity
+    assert hash_file(filename) == b'=\x9e\x1f\xec\xa6\x81\xc1\xd84\xbd\x0b?.\x1b\x8ew'
+    num_lines = 58070492
+
+
+    # get chunk of data
+    
